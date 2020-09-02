4565c30ddced36a2311ba005fba2b83d8e9da98e
Marco Hijacker <marco.longo@hotmail.it>
2020-09-02 09:38:02 +0200

    Versione completa.

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 $paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+$lenght = strlen($paragraph);
 $badword = $_GET["name"];
 $censoredpar = str_replace($badword, "***", $paragraph);
 ?>
 
 <p><u>Stringa originale:</u> <?php echo $paragraph; ?></p>
+<p><u>Lunghezza stringa originale:</u> <?php echo $lenght; ?></p>
 <p><u>Bad word:</u> <?php echo $badword; ?></p>
 <p><u>Stringa censurata:</u> <?php echo $censoredpar; ?></p>
